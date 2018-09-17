02490e2cf9c0f5ab85367ef8472110f25268d14e
Akelsandr Saltykov <oleksandr.saltykov@globant.com>
2018-09-17 06:21:37 +0300

    §

---
+++ activemq/src/main/java/com/example/activemq/Listener.java
@@ -0,0 +1,44 @@
+package com.example.activemq;
+
+import com.google.gson.Gson;
+import org.springframework.jms.annotation.JmsListener;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Component;
+
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.TextMessage;
+import java.util.Map;
+
+@Component
+public class Listener {
+    @JmsListener(destination = "inbound.queue")
+    @SendTo("outbound.queue")
+    public String receiveMessage(final Message jsonMessage) throws JMSException {
+        String messageData = null;
+        System.out.println("Received message " + jsonMessage);
+        String response = null;
+        if(jsonMessage instanceof TextMessage) {
+            TextMessage textMessage = (TextMessage)jsonMessage;
+            messageData = textMessage.getText();
+            Map map = new Gson().fromJson(messageData, Map.class);
+            response  = "Hello " + map.get("name");
+        }
+        return response;
+    }
+
+    @JmsListener(destination = "inbound.topic")
+    @SendTo("outbound.topic")
+    public String receiveMessageFromTopic(final Message jsonMessage) throws JMSException {
+        String messageData = null;
+        System.out.println("Received message " + jsonMessage);
+        String response = null;
+        if(jsonMessage instanceof TextMessage) {
+            TextMessage textMessage = (TextMessage)jsonMessage;
+            messageData = textMessage.getText();
+            Map map = new Gson().fromJson(messageData, Map.class);
+            response  = "Hello " + map.get("name");
+        }
+        return response;
+    }
+}
